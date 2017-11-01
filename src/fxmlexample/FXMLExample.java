9042c11286249c52afa0790b7e30e4ff2b46237b
Adam Forbis <aforbis83091@gmail.com>
2017-10-31 20:26:46 -0500

    Add text and password fields

--- src/fxmlexample/FXMLExample.java
+++ src/fxmlexample/FXMLExample.java
@@ -21,8 +21,9 @@ public class FXMLExample extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
         
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 300, 275);
         
+        stage.setTitle("FXML Welcome");
         stage.setScene(scene);
         stage.show();
     }
